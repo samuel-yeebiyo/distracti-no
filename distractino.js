e567868e1996c54a4cd53b60ea4b7ebb58a6b504
samuel-yeebiyo <samoyemaneyy@gmail.com>
2022-11-11 20:47:52 +0000

    Commented unnecessary console logs

--- distractino.js
+++ distractino.js
@@ -5,17 +5,16 @@ const config = {attributes: true, childList: true, subTree: true}
 const callback = (mutationList, observer) =>{
     for(const mutation of mutationList){
         if(mutation.type === 'childList'){
-            console.log("child node added or removed")
             const skipButton = document.querySelector(".ytp-ad-skip-button")
             if(skipButton){
-                console.log("Found skip button")
                 skipButton.click()
             }
-        }else if(mutation.type === 'attributes'){
+        }
+	/*else if(mutation.type === 'attributes'){
             console.log(`The ${mutation.attributeName} attribute was changed`)
         }else{
             console.log("something else changed")
-        }
+        }*/
     }
 }
 
